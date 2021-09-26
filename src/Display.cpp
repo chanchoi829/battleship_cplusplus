3eb4d68944be99b5e39af9c1e4bbf5ae7dfe0ff7
Chan Choi <chanchoi829@beyless.com>
2021-09-26 13:02:19 +0900

    CMakeLists.txt added

--- src/Display.cpp
+++ src/Display.cpp
@@ -14,10 +14,10 @@ using namespace cimg_library;
 
 Display_::Display_() {
     // Load images
-    sea_img = CImg<unsigned char> ("sea.jpg");
-    ship_img = CImg<unsigned char> ("ship.jpg");
-    black_img = CImg<unsigned char> ("black.jpg");
-    red_img = CImg<unsigned char> ("red.jpg");
+    sea_img = CImg<unsigned char> ("./images/sea.jpg");
+    ship_img = CImg<unsigned char> ("./images/ship.jpg");
+    black_img = CImg<unsigned char> ("./images/black.jpg");
+    red_img = CImg<unsigned char> ("./images/red.jpg");
 }
 
 void Display_::draw() {
